67a40f2d9a0325f7e11960cc7a4a88547286ee6c
vladimirbabaskin <vladimirbabaskin@gmail.com>
2019-03-06 13:45:03 +0200

    Edited the DB and the `.gitignore` file

---
+++ database/migrations/2019_03_06_113735_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->string("title", 127);
+            $table->text("text");
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
